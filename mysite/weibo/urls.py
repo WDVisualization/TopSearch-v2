39dddf6049a657153bfd8a73b687cee0b6d1a8a6
qianwenhui <648471418@qq.com>
2020-04-14 23:48:27 +0800

    更新

--- mysite/weibo/urls.py
+++ mysite/weibo/urls.py
@@ -2,7 +2,11 @@ from django.urls import path
 from . import views
 urlpatterns = [
     path('', views.index, name = 'index'),
-    
+    path('analysis', views.analysisview, name='analysisview'),
+
     path('lists', views.lists, name = 'lists'),
     path('wdcld', views.wdcld, name = 'wdcld'),
+    path('toptwenty', views.toptwenty, name='toptwenty'),
+    path('changelists', views.changelists, name='changelists'),
+
 ]
--- mysite/weibo/views.py
+++ mysite/weibo/views.py
@@ -22,6 +22,8 @@ def index(request):
     # api = json.loads(api_request.content)
     # return render(request,'index.html', {"api": api})
     return render(request, 'index.html', {"data": data})
+
+
 # 词云
 def wdcld(request):
     try:
@@ -46,9 +48,9 @@ def wdcld(request):
         # 次数排序
         telists = list(te.items())
         telists.sort(key=lambda x: x[1], reverse=True)
-        # 输出次数前TOP10的词语
+        # 输出次数前TOP20的词语
         ranklist = []
-        for i in range(0, 10):
+        for i in range(0, 20):
             print(telists[i])
             ranklist.append(telists[i])
 
@@ -77,7 +79,6 @@ def lists(request):
         objects_list = []
         for data in datas:
             d = collections.OrderedDict()
-            # d['id'] = data[0]
             d['rank'] = data[1]
             d['keywords'] = data[2]
             d['flow'] = data[3]
@@ -86,6 +87,56 @@ def lists(request):
             # print(objects_list)
 
         res = {'msg': '查询数据成功', 'code': 400, 'data': len(objects_list)}
+    except:
+        print('出现错误')
+        res = {'msg': '查询失败', 'code': 200}
+    return HttpResponse(json.dumps(res, ensure_ascii=False))
+
+
+def analysisview(request):
+    return render(request, 'toptwenty_analysis.html')
+
+
+def toptwenty(request):
+    try:
+        sql = "SELECT * FROM `weibo` where rank=1 group by keywords ORDER BY flow DESC limit 0,20"
+        cursor.execute(sql)
+        datas = cursor.fetchall()
+        db.commit()
+        objects_list = []
+        for data in datas:
+            d = collections.OrderedDict()
+            d['keywords'] = data[2]
+            d['flow'] = data[3]
+            d['url'] = data[4]
+            objects_list.append(d)
+            # print(objects_list)
+
+        res = {'msg': '查询数据成功', 'code': 400, 'data': objects_list}
+    except:
+        print('出现错误')
+        res = {'msg': '查询失败', 'code': 200}
+    return HttpResponse(json.dumps(res, ensure_ascii=False))
+
+
+def changelists(request):
+    try:
+        kwd = request.GET['keywords']
+        if len(kwd) > 0:
+            sql = "SELECT * FROM `weibo` where keywords = "+kwd+" ORDER BY created_at"
+            cursor.execute(sql)
+            datas = cursor.fetchall()
+            db.commit()
+        objects_list = []
+        for data in datas:
+            d = collections.OrderedDict()
+            d['keywords'] = data[2]
+            d['flow'] = data[3]
+            d['url'] = data[4]
+            d['created_at'] = str(data[5])
+            objects_list.append(d)
+            # print(objects_list)
+        res = {'msg': '查询数据成功', 'code': 400, 'data': objects_list}
     except:
         print('出现错误')
         res = {'msg': '查询失败', 'code': 200}
